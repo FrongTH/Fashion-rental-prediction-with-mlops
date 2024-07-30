9140929946e927a0b21b1eb1ef791aa4af1eed45
Frong <rattapon.san@gmail.com>
2024-07-31 00:19:46 +0700

    init

--- script/ingest.py
+++ script/ingest.py
@@ -1,11 +1,40 @@
 import pandas as pd
 import yaml
 import argparse
+from datetime import datetime
+import os
+datetimerun = datetime.now().strftime("%Y-%m-%d-%H-%M")
 
 parser = argparse.ArgumentParser(description="Read data from a CSV file using configuration file.")
 parser.add_argument('--config', type=str, default='../config.yaml', help="Path to the configuration YAML file.")
 
-def read_data(config_path: str = parser.parse_args().config) -> pd.DataFrame:
+def save_ingestion(df, config):
+    """
+    Save the ingestion DataFrame to a specified directory structure.
+    
+    Parameters:
+    - df (pd.DataFrame): The DataFrame to save.
+    - config (dict): Configuration dictionary containing 'WORK_DIR' and 'INGESTION_PATH'.
+    
+    Returns:
+    - None
+    """
+    datetimerun = datetime.now().strftime("%Y-%m-%d-%H-%M")
+    ingest_saved_path = os.path.join(config['WORK_DIR'], config['INGESTION_PATH'], datetimerun)
+    
+    # Create the ingestion path if it does not exist
+    if not os.path.exists(os.path.join(config['WORK_DIR'], config['INGESTION_PATH'])):
+        os.makedirs(os.path.join(config['WORK_DIR'], config['INGESTION_PATH']))
+        
+    # Save the DataFrame if the ingest_saved_path does not exist
+    if not os.path.exists(ingest_saved_path):
+        os.makedirs(ingest_saved_path)
+        df.to_csv(os.path.join(ingest_saved_path, 'ingest.csv'), index=False)
+        print('##### Ingestion is saved to {}'.format(os.path.join(ingest_saved_path, 'ingest.csv')))
+    else:
+        print(f'{ingest_saved_path} already exists!')
+
+def read2save_data(config_path: str = parser.parse_args().config) -> pd.DataFrame:
     """
     Reads data from a CSV file specified in the configuration file.
 
@@ -32,9 +61,9 @@ def read_data(config_path: str = parser.parse_args().config) -> pd.DataFrame:
 
     data_path = config['DATA_PATH']
     df = pd.read_csv(data_path)
-    return df
+    save_ingestion(df, config)
 
-# if __name__ == '__main__':
-#     args = parser.parse_args()
+if __name__ == '__main__':
+    args = parser.parse_args()
+    read2save_data(args.config)
     
-#     df = read_data(args.config)
